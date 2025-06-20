59216ce0f7b0438ba2e51933fecb56166302014c
dEitY719 <deity719@naver.com>
2025-06-20 14:40:15 +0900

    chore: update .gitconfig

---
+++ bash/app/uv.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# bash/app/uv.bash
+
+alias uv_install='uv sync'
+alias uv_update='uv sync --upgrade'
+alias uv_upgrade='uv sync --upgrade'
+alias uv_clean='uv sync --clean'
+alias uv_check='uv check'
+alias uv_lock='uv lock'
+alias uv_unlock='uv unlock'
+
+
+uv_sync(){
+    log_dim "Installing [project & development] dependencies..."
+    uv sync
+    uv sync --dev --extra dev
+    log_info "[project & development] dependencies installed"
+}
+
+
+uv_dev(){
+    log_dim "Installing [development] dependencies..."
+    uv sync --dev --extra dev
+    log_info "[development] dependencies installed"
+}
+
+
+uv_prod(){
+    log_dim "Installing [production] dependencies..."
+    uv sync
+    log_info "[production] dependencies installed"
+}
