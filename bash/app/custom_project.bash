3ccf0fb37d504e6e489c50f25dc495ae57b1c9b9
dEitY719 <deity719@naver.com>
2025-09-17 08:41:49 +0900

    alias: run_bes for dmc-playground backend server

---
+++ bash/app/custom_project.bash
@@ -0,0 +1,4 @@
+# FinRx project
+# dmc-playground project
+alias run_bes="uv run uvicorn src.backend.main:app --reload --host 127.0.0.1 --port 8000"
+# alias run_fes="uv run uvicorn src.backend.main:app --reload --host 127.0.0.1 --port 8000"
