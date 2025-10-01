2cd7e07a20b3fb514a185942cec4bb32b5fdeb29
dEitY719 <deity719@naver.com>
2025-10-01 11:23:27 +0900

    alias: .../bash/app/custom_project.bash

--- bash/app/custom_project.bash
+++ bash/app/custom_project.bash
@@ -1,4 +1,20 @@
-# FinRx project
-# dmc-playground project
+#======================================
+# dotfiles
+#       ✨ https://github.com/dEitY719/dotfiles.git
+#======================================
+# myh
+# cd_dot
+
+#======================================
+# FinRx
+#       💰 https://github.com/dEitY719/FinRx.git
+#======================================
+# TBD ...
+
+#======================================
+# dmc-playground
+#       📚 https://github.com/dEitY719/dmc-playground.git
+#======================================
 alias run_bes="uv run uvicorn src.backend.main:app --reload --host 127.0.0.1 --port 8000"
 # alias run_fes="uv run uvicorn src.backend.main:app --reload --host 127.0.0.1 --port 8000"
+alias run_cli="python api_cli.py"
