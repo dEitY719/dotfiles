7e8b8839456703ac280304ae3f2ce286fb54f5cd
dEitY719 <deity719@naver.com>
2025-10-01 19:40:06 +0900

    alias: update run_fcli

--- bash/app/custom_project.bash
+++ bash/app/custom_project.bash
@@ -10,7 +10,7 @@
 #       💰 https://github.com/dEitY719/FinRx.git
 #======================================
 # TBD ...
-alias run_fcli='python ./src/ticker_library/cli.py'
+alias run_fcli='python ./src/ticker_library/cli/cli.py'
 
 #======================================
 # dmc-playground
