2e19861b3a68e0cd211cc29ed501a7550c92bb5f
dEitY719 <deity719@naver.com>
2025-10-17 15:03:01 +0900

    feat: check_port

--- bash/coreutils/kill.bash
+++ bash/coreutils/kill.bash
@@ -19,3 +19,18 @@ kill_by_port() {
 
 # alias 로 간단하게 호출할 수 있게 정의
 alias kp=kill_by_port
+
+
+# 포트 확인 함수
+check_port () {
+    if [ -z "$1" ]; then
+        echo "사용법: check_port <포트번호>"
+        return 1
+    fi
+    PORT=$1
+    echo "🔍 포트 $PORT 사용 현황:"
+    lsof -i :"$PORT" || echo "해당 포트를 사용하는 프로세스가 없습니다."
+}
+
+# 짧은 별칭
+alias lp='check_port'
